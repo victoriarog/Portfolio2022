b8d3b3af68b0fe84aa4ad4e71916e59e4024b8de
Vicka <vicrogatinskaya@gmail.com>
2023-02-03 12:36:33 +0100

    single page now

--- portfolio-web/pages/index.vue
+++ portfolio-web/pages/index.vue
@@ -1,8 +1,45 @@
 <template>
-  <Header />  
-  <img src="../assets/images/head.png" class="indeximg">
-  <div class="text">
-    <h1>HI, my name is Victoria</h1>
-    <p>front end developer, tech enthusiast, and a bit artist</p>
+
+<div class="nav">
+    <button>
+      <a href="#top">Home?</a>
+    </button>
+    <button>
+      <a href="#about">About</a>
+    </button>
+    <button>
+      <a href="#projects">Projects</a>
+    </button>
+    <button>
+      <a href="#contact">Contact</a>
+    </button> 
   </div>
+
+  <div id="top">
+    <img src="../assets/images/head.png" class="indeximg">
+    <div class="text">
+      <h1>HI, my name is Victoria</h1>
+      <p>front end developer, tech enthusiast, and a bit artist</p>
+    </div>
+  </div>
+
+  <div id="about">
+    <h1>ABOUT ME</h1> 
+</div>
+
+<div id="projects">
+    <h1>PROJECTS</h1>
+</div>
+
+<div id="contact">
+    <h1>CONTACT ME</h1>
+    <!-- <div>
+        <a href="/">LinkedIn</a>
+    </div> -->
+    <div>
+        <input class="emailin" type="email" placeholder="Your e-mail"/><br/><br/>
+        <input class="msgin" type="text" placeholder="Drop me a message!"/>
+    </div>
+</div>
+
 </template>
